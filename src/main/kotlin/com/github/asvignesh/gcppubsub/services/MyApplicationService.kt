7735def837d0f345da100deea27cd532582165a4
GitHub Action <action@github.com>
2022-07-14 05:31:26 +0000

    Template cleanup

--- src/main/kotlin/com/github/asvignesh/gcppubsub/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/asvignesh/gcppubsub/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.asvignesh.gcppubsub.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.asvignesh.gcppubsub.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/asvignesh/gcppubsub/services/MyProjectService.kt
+++ src/main/kotlin/com/github/asvignesh/gcppubsub/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.asvignesh.gcppubsub.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.asvignesh.gcppubsub.MyBundle
 
 class MyProjectService(project: Project) {
 
